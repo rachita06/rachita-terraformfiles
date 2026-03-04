20894df9f0f0a259cf82b08baa9ba5fab489cadf
rachita <>
2026-03-04 14:31:39 +0545

    ec2 connected

---
+++ Ec2.py
@@ -0,0 +1,14 @@
+# Create EC2 Instance
+resource "aws_instance" "my_ec2" {
+  ami                         = "ami-0c02fb55956c7d316"  # Amazon Linux 2 (us-east-1)
+  instance_type               = "t3.micro"
+  subnet_id                   = aws_subnet.my_subnet.id
+  vpc_security_group_ids      = [aws_security_group.my_sg.id]
+  associate_public_ip_address = true
+
+  tags = {
+    Name = "rachita-terraform-ec2"
+    cost ="Sales"
+  }
+key_name = "rachita-key02"
+user_data = <<-EOF
