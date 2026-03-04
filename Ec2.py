364381475150ac6ed582004c2cef2bfeb07d368b
rachita <>
2026-03-04 14:59:43 +0545

    modified

--- Ec2.py
+++ Ec2.py
@@ -5,6 +5,7 @@ resource "aws_instance" "my_ec2" {
   subnet_id                   = aws_subnet.my_subnet.id
   vpc_security_group_ids      = [aws_security_group.my_sg.id]
   associate_public_ip_address = true
+#tags are here
 
   tags = {
     Name = "rachita-terraform-ec2"
