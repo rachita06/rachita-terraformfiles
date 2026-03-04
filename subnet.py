bab270eb4248a94dfe8a1783e992ea6fd995f79b
rachita <>
2026-03-04 14:19:39 +0545

    second commit

--- subnet.py
+++ subnet.py
@@ -1,10 +1,10 @@
-# Create VPC
-resource "aws_vpc" "my_vpc" {
-  cidr_block           = "10.0.0.0/16"
-  enable_dns_support   = true
-  enable_dns_hostnames = true
+# Create Subnet
+resource "aws_subnet" "my_subnet" {
+  vpc_id            = aws_vpc.my_vpc.id
+  cidr_block        = "10.0.1.0/24"
+  availability_zone = "us-east-1a"
 
   tags = {
-    Name = "rachita-terraform-vpc"
+    Name = "rachita-terraform-subnet"
   }
 }
